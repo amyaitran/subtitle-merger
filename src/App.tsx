6146935636203514f1e901615570acd9f5c9c6f6
Jimmy Tran <jimmyytran@gmail.com>
2020-05-12 01:45:19 -0700

    add more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import { SearchBar, Stepper, Paper, Container } from './components';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header"></header> */}
       <SearchBar />
+      <Stepper />
+      <Paper />
     </div>
   );
 }
